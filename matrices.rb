418dae92c6c95db315058a0b43b7f16db093e675
kubilaycaglayan <kblycaglayan@gmail.com>
2024-11-28 17:40:05 +0300

    Implement matrix multiplication

--- matrices.rb
+++ matrices.rb
@@ -1,4 +1,26 @@
 def multiply_matrices(m1, m2)
-  return 0
-end
+  return if m1[0].length != m2.length
 
+  result = []
+
+  m1.each_index do |m1_row_index|
+    m1_row = m1[m1_row_index]
+
+    m2[0].each_index do |m2_col_index|
+      sum_series = []
+
+      m1_row.each_index do |m1_el_index|
+        m1_element = m1_row[m1_el_index]
+        m2_element = m2[m1_el_index][m2_col_index]
+
+        multiplication_result = m1_element * m2_element
+        sum_series << multiplication_result
+      end
+
+      result[m1_row_index] = [] if result[m1_row_index].nil?
+      result[m1_row_index] << sum_series.sum
+    end
+  end
+
+  result
+end
--- matrices_spec.rb
+++ matrices_spec.rb
@@ -3,10 +3,74 @@ require "rspec"
 require_relative "matrices"
 
 describe "multiply_matrices" do
-  it "multiplies two given matrices and returns the result" do
-    m1 = [[1, 2], [3, 4]]
-    m2 = [[5, 6], [7, 8]]
+  describe "multiplies two given matrices and returns the result" do
+    it "example 1" do
+      m1 = [
+        [1, 2],
+        [3, 4]
+          ]
+      m2 = [
+        [5, 6],
+        [7, 8]
+      ]
 
-    expect(multiply_matrices(m1, m2)).to eq([[19, 22], [43, 50]])
+      expect(multiply_matrices(m1, m2)).to eq([[19, 22], [43, 50]])
+    end
+
+    it "example 2" do
+      m1 = [
+        [3, 1, 4]
+      ]
+      m2 = [
+        [4,3],
+        [2,5],
+        [6,8]
+      ]
+
+      expect(multiply_matrices(m1,m2)).to eq([[38,46]])
+    end
+
+    it "example 3" do
+      m1 = [
+        [1, 2, 3],
+        [4, 5, 6]
+      ]
+      m2 = [
+        [7, 8],
+        [9, 10],
+        [11, 12]
+      ]
+
+      expect(multiply_matrices(m1,m2)).to eq([[58, 64], [139, 154]])
+    end
+
+    it "example 4" do
+      m1 = [
+        [1, 2],
+        [3, 4],
+        [5, 6]
+      ]
+      m2 = [
+        [7, 8, 9],
+        [10, 11, 12]
+      ]
+
+      expect(multiply_matrices(m1,m2)).to eq([[27, 30, 33], [61, 68, 75], [95, 106, 117]])
+    end
+
+    it "example 5" do
+      m1 = [
+        [1, 2, 3],
+        [4, 5, 6],
+        [7, 8, 9]
+      ]
+      m2 = [
+        [1, 0, 0],
+        [0, 1, 0],
+        [0, 0, 1]
+      ]
+
+      expect(multiply_matrices(m1,m2)).to eq([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    end
   end
 end
